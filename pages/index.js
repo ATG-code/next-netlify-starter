71dc7f7d942b7fc5d132cdc1699178fbf13de610
ATG <55929739+ATG-code@users.noreply.github.com>
2024-05-22 22:21:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1 className={style.title}>Hasan ile Date</h1>
       </main>
-
-      <Footer />
     </div>
   )
 }
