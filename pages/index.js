279a318fa9262a9456b5aeebb913761150cfdafc
ATG <55929739+ATG-code@users.noreply.github.com>
2024-05-24 19:42:20 +0300

    0

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <div class="bouncing-blobs-container">
-          <div class="bouncing-blobs-glass"></div>
-          <div class="bouncing-blobs">
-            <div class="bouncing-blob bouncing-blob--blue"></div>
-            <div class="bouncing-blob bouncing-blob--blue"></div>
-            <div class="bouncing-blob bouncing-blob--blue"></div>
-            <div class="bouncing-blob bouncing-blob--white"></div>
-            <div class="bouncing-blob bouncing-blob--purple"></div>
-            <div class="bouncing-blob bouncing-blob--purple"></div>
-            <div class="bouncing-blob bouncing-blob--pink"></div>
-          </div>
-        </div>
-        <div class="hero">
-          <h2>Hasan Seninle Bir Yaşam Sonsuz Bir Roman</h2>
-          <p>It works by adding blurred blobs that bounce off the screen edge</p>
-        </div>
+        <div className='container'></div>
       </main>
     </div>
   )
